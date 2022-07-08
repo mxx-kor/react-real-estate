918c0efbcf8b0721b33b393e9262b6611cabad74
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-07-08 15:56:26 +0900

    dotenv 설치 및 규칙 따름

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -2,13 +2,11 @@ import axios from "axios";
 
 export const baseUrl = 'https://bayut.p.rapidapi.com'
 
-// headers: {'X-RapidAPI-Key': '20fd69a613mshfb876fe5b78d611p107431jsn50062e01807a', 'X-RapidAPI-Host': 'bayut.p.rapidapi.com'}
-
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
       headers: {
         'x-rapidapi-host': 'bayut.p.rapidapi.com',
-        'x-rapidapi-key': process.env.REACT_API_KEY,
+        'x-rapidapi-key': process.env.REACT_APP_APIKEY,
       },
     });
       
